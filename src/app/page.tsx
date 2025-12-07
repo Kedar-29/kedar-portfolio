301f1c3b9616efe44a639c457d780d1443f61f38
Kedar Mhetre <kedarsm2020@gmail.com>
2025-12-07 23:36:20 +0530

    new updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,35 +11,28 @@ import ProjectSection from "@/components/ProjectSection";
 export default function Home() {
   return (
     <div className="scroll-smooth">
-      {/* NAVBAR */}
       <NavBarItem />
 
-      {/* HERO */}
       <section id="home" className="scroll-mt-24">
         <HeroSection />
       </section>
 
-      {/* ABOUT */}
       <section id="about" className="scroll-mt-24">
         <AboutSection />
       </section>
 
-      {/* EXPERIENCE */}
       <section id="experiance" className="scroll-mt-24">
         <ExperienceSection />
       </section>
 
-      {/* PROJECTS */}
       <section id="projects" className="scroll-mt-24">
         <ProjectSection />
       </section>
 
-      {/* CONTACT */}
       <section id="contact" className="scroll-mt-24">
         <ContactSection />
       </section>
 
-      {/* FOOTER */}
       <Footersectiom />
     </div>
   );
